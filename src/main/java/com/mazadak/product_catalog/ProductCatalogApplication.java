fb2f546ebcac3abede0f55dfccbc28eba3694378
yousef371 <youseffff925@gmail.com>
2025-10-08 14:07:44 +0300

    Intalize project

--- src/main/java/com/mazadak/product_catalog/ProductCatalogApplication.java
+++ src/main/java/com/mazadak/product_catalog/ProductCatalogApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductCatalogApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCatalogApplication.class, args);
 	}

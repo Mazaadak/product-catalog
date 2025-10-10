fa4ceb681e1cc073ca02f1e842d936187a52ce5f
yousef371 <youseffff925@gmail.com>
2025-10-10 14:36:29 +0300

    ADD Product enums

---
+++ src/main/java/com/mazadak/product_catalog/entities/ProductStatus.java
@@ -0,0 +1,8 @@
+package com.mazadak.product_catalog.entities;
+
+public enum ProductStatus {
+    ACTIVE,
+    INACTIVE,
+    DELETED
+}
+
---
+++ src/main/java/com/mazadak/product_catalog/entities/ProductType.java
@@ -0,0 +1,6 @@
+package com.mazadak.product_catalog.entities;
+
+public enum ProductType {
+    AUCTION,
+    FIXED
+}

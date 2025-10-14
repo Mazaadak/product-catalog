ba8c900105f5513a578dfc04582e20029c9b5df9
yousef371 <youseffff925@gmail.com>
2025-10-14 16:03:42 +0300

    Add @Repository annotation to OutboxEventRepository

--- src/main/java/com/mazadak/product_catalog/repositories/OutboxEventRepository.java
+++ src/main/java/com/mazadak/product_catalog/repositories/OutboxEventRepository.java
@@ -2,8 +2,10 @@ package com.mazadak.product_catalog.repositories;
 
 import com.mazadak.product_catalog.entities.OutboxEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface OutboxEventRepository extends JpaRepository<OutboxEvent, UUID> {
 }

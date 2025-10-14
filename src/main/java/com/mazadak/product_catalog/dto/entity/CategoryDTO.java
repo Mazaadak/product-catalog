a7b38546b50de1b5f31bf88ab665fe414b07545a
yousef371 <youseffff925@gmail.com>
2025-10-14 14:50:11 +0300

    Add CategoryDTO and CategoryMapper using MapStruct
    Introduce CategoryDTO and a MapStruct mapper interface with methods to convert between Category and CategoryDTO and their list variants.

---
+++ src/main/java/com/mazadak/product_catalog/dto/entity/CategoryDTO.java
@@ -0,0 +1,11 @@
+package com.mazadak.product_catalog.dto.entity;
+
+import lombok.Data;
+
+
+@Data
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String description;
+}

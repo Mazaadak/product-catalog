2bc7d60f7ac034f512f71cdb05e57bf90925ce15
yousef371 <youseffff925@gmail.com>
2025-10-14 15:26:44 +0300

    Add ProductUpdatedEvent and ProductDeletedEvent DTOs for product change events

---
+++ src/main/java/com/mazadak/product_catalog/dto/event/ProductDeletedEvent.java
@@ -0,0 +1,14 @@
+package com.mazadak.product_catalog.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDeletedEvent {
+    private Long productId;
+}
---
+++ src/main/java/com/mazadak/product_catalog/dto/event/ProductUpdatedEvent.java
@@ -0,0 +1,21 @@
+package com.mazadak.product_catalog.dto.event;
+
+import com.mazadak.product_catalog.entities.enums.ProductType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductUpdatedEvent {
+    private Long productId;
+    private Long sellerId;
+    private String title;
+    private String description;
+    private BigDecimal price;
+    private ProductType type;
+    private Long categoryId;
+}
